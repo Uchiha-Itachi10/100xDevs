3f991bc239a693127c8339d350aa363463f5f718
Hari Sai <71338174+Uchiha-Itachi10@users.noreply.github.com>
2026-05-20 22:57:02 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomImage1 = "dice" + randomNumber1 + ".png";
+var randomSource1 = "images/" + randomImage1;
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImage2 = "dice" + randomNumber2 + ".png";
+var randomSource2 = "images/" + randomImage2;
+
+document.querySelectorAll("img")[0].setAttribute("src", randomSource1);
+document.querySelectorAll("img")[1].setAttribute("src", randomSource2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 wins! YAYY!!!🏆";
+} else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 wins! YAYY!!!🏆";
+} else{
+    document.querySelector("h1").innerHTML = "Draw😪";
+}
+
